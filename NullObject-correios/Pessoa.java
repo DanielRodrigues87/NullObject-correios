40c21a3e6dd9a9a8f8488dd20c7552c0c976cf5b
DanielRodrigues87 <67766681+DanielRodrigues87@users.noreply.github.com>
2022-09-22 15:35:39 -0300

    Primeiro commit

---
+++ NullObject-correios/Pessoa.java
@@ -0,0 +1,41 @@
+public class Pessoa {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    private String sobrenome;
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    
+    private Endereco endereco;
+    
+
+
+    public Pessoa(String nome, String sobrenome ) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    
+}
---
+++ NullObject-correios/PessoaFactory.java
@@ -0,0 +1,14 @@
+public class PessoaFactory {
+   
+    public static Pessoa criarPessoa(String nome, String sobrenome, 
+    Endereco endereco){
+        Pessoa pessoa = new Pessoa(nome, sobrenome);
+        if (endereco == null) {
+            pessoa.setEndereco(new EnderecoNull());
+        } else {
+            pessoa.setEndereco(endereco);
+        }
+        return pessoa;
+
+    }
+}
